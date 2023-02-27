df3dcbc266dc01310be2d43693da0e3e64805ce9
r2dev2 <ronak.badhe@gmail.com>
2023-02-27 14:10:13 -0800

    speeeeeeedy writing owo

--- beethboard.ino
+++ beethboard.ino
@@ -1,16 +1,24 @@
 #define MIC_READ_PIN A0
+#define BUFF_SIZE 256
 
-int mic_read = 0;
-unsigned long timestamp = 0;
+int mic_read[BUFF_SIZE] = {0};
+unsigned long timestamp[BUFF_SIZE] = {0};
 
 void setup() {
-	Serial.begin(9600);
+	Serial.begin(115200);
+	ADCSRA = (ADCSRA & 0xf8) | 0x04; // yea idk wtf this does it makes it fast :shrug:
 }
 
 void loop() {
-	mic_read = analogRead(MIC_READ_PIN);
-	timestamp = millis();
-	Serial.print(timestamp);
-	Serial.print(",");
-	Serial.println(mic_read);
+	for (int i = 0; i < BUFF_SIZE; ++i) {
+		mic_read[i] = analogRead(MIC_READ_PIN);
+		timestamp[i] = micros();
+	}
+
+	for (int i = 0; i < BUFF_SIZE; ++i) {
+		Serial.write((uint8_t *) &timestamp[i], sizeof(unsigned long) / sizeof(char));
+		Serial.write((uint8_t *) &mic_read[i], sizeof(int) / sizeof(char));
+	}
+	Serial.println();
+	Serial.flush();
 }
--- src/beethboard.py
+++ src/beethboard.py
@@ -1,22 +1,39 @@
 from contextlib import suppress
+from struct import unpack, calcsize
 import time
 import os
 import serial
 
-port = 9600
+buff_size = 256
+port = 115200
 device = os.environ.get("ARDUINO_BEETHBOARD_PORT", "/dev/ttyACM0")
 freq = [1] * 500
+data_fmt = "<Lh"
+data_size = calcsize(data_fmt)
+
+def parse_line(line: bytes):
+    if len(line) != buff_size * data_size + 2:
+        return []
+
+    # collect each entry into line
+    line = []
+    for i in range(buff_size):
+        start = i * data_size
+        end = start + data_size
+        line.append(unpack(data_fmt, line[start:end]))
+    return line
 
 def main():
     with serial.Serial(device, port) as ser:
         beg = time.time()
         for i, line in enumerate(ser):
-            with suppress(Exception):
-                line = line.decode().strip()
-                timestamp, mic_val = map(int, line.split(","))
-                dt = (timestamp - freq[mic_val]) / 1000
-                print(timestamp, mic_val, int(1/dt))
-                freq[mic_val] = timestamp
+            dur = time.time() - beg
+            if i % (1000 // buff_size) == 0:
+                print(i, dur, buff_size * i / dur)
+                # timestamp, mic_val = map(int, line.split(","))
+                # dt = (timestamp - freq[mic_val]) / 1000
+                # print(timestamp, mic_val, int(1/dt))
+                # freq[mic_val] = timestamp
 
 
 if __name__ == "__main__":
